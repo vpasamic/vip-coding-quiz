3f5e75bb605519cb0471d38d4dc7591ab16c7f3b
Ver <vpasamic22@gmail.com>
2020-09-16 18:24:55 -0700

    added questionair buttons and questions. added questions variable, added necessary functions from pseudocode

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+
+//dom variables
 var startdiv = document.getElementById("starter")
 var startbtn = document.getElementById("startbtn")
 var starthsbtn = document.getElementById("starths")
+var timerdiv = document.getElementById("timer")
+//global variables
+var questions = {
 
+ }
+
+var score = 0;
+var timer = 10;
+
+
+function settime() {
+    //time ticks
+    var timerInterval = setInterval(function(){
+        timer--;
+        timerdiv.textContent = timer;
+    //if timer ends
+        if (timer <= 0) {
+        clearInterval(timerInterval)
+    }
+  },1000);
+}
+// settime()
 function quizstart(){
-
+    console.log("hi")
+    settime()
 
 }
 
 function showhs(){
 
-    
+
 }
 
 startbtn.addEventListener("click", quizstart)
