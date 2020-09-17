c20c9583955fb90a31f5983f05238d9f793a8cdf
Ver <vpasamic22@gmail.com>
2020-09-16 18:42:06 -0700

    added working buttons a b c and d buttons. buttons now perform check fucntion to generate new questions)

--- script.js
+++ script.js
@@ -4,14 +4,34 @@ var startdiv = document.getElementById("starter")
 var startbtn = document.getElementById("startbtn")
 var starthsbtn = document.getElementById("starths")
 var timerdiv = document.getElementById("timer")
+var abtn =document.getElementById("a")
+var bbtn=document.getElementById("b")
+var cbtn=document.getElementById("c")
+var dbtn=document.getElementById("d")
+var questiondiv=document.getElementById("question")
 //global variables
-var questions = {
-
- }
+var questions = 
+[{
+    question:"Which HTML element does javascript go into", 
+    a:"<js>",
+    b:"<script>",
+    c:"<scripting>",
+    d:"<javascript>",
+    correctanswer:"b",
+},
+{
+    question:"how to get a random number from 0 to 10",
+    a:"Math.Random(0,10)",
+    b:"Math.Floor(Math.Random()*10)",
+    c:"Math.floor(Math.random()*10)",
+    d:"Math.floor(Math.random(0,10)",
+    correctanswer:"c",
+}]
 
 var score = 0;
-var timer = 10;
-
+var timer = questions.length*10;
+var questionnumber=0
+var correct;
 
 function settime() {
     //time ticks
@@ -28,13 +48,43 @@ function settime() {
 function quizstart(){
     console.log("hi")
     settime()
+    generate()
 
 }
+//generates question
+function generate(){
+    var currentquestions=questions[questionnumber]
+    questiondiv.innerHTML = "<p>" + currentquestions.question + "</p>"
+    abtn.textContent=currentquestions.a
+    bbtn.textContent=currentquestions.b
+    cbtn.textContent=currentquestions.c
+    dbtn.textContent=currentquestions.d
+}
+//check answers
+function checka (){
+    generate()
+    questionnumber++
+}
 
+function checkb (){
+    generate()
+    questionnumber++
+}
+function checkc (){
+    generate()
+    questionnumber++
+}
+function checkd (){
+    generate()
+    questionnumber++
+}
 function showhs(){
 
 
 }
-
+abtn.addEventListener("click", checka)
+bbtn.addEventListener("click", checkb)
+cbtn.addEventListener("click", checkc)
+dbtn.addEventListener("click", checkd)
 startbtn.addEventListener("click", quizstart)
 starthsbtn.addEventListener("click", showhs)
