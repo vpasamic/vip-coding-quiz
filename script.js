c07291a793f8a13e71f8dbc8694ef26660a22161
Ver <vpasamic22@gmail.com>
2020-09-16 17:34:44 -0700

    added wowrking buttons to start page

---
+++ script.js
@@ -0,0 +1,16 @@
+var startdiv = document.getElementById("starter")
+var startbtn = document.getElementById("startbtn")
+var starthsbtn = document.getElementById("starths")
+
+function quizstart(){
+
+
+}
+
+function showhs(){
+
+    
+}
+
+startbtn.addEventListener("click", quizstart)
+starthsbtn.addEventListener("click", showhs)
